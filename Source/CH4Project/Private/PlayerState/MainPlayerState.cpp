12ccc2431a873c4ff5da78e2d9665d3259b39d94
LeeSh-213 <hasanghyun66@naver.com>
2025-09-09 19:52:05 +0900

    게임모드 및 다른 파트 복구, 추후 명칭 수정 예정

--- Source/CH4Project/Private/PlayerState/MainPlayerState.cpp
+++ Source/CH4Project/Private/PlayerState/MainPlayerState.cpp
@@ -3,20 +3,33 @@
 
 #include "PlayerState/MainPlayerState.h"
 #include "Net/UnrealNetwork.h"
-
-
 AMainPlayerState::AMainPlayerState()
 {
-	PlayerRole = EPlayerRole::Unassigned ; // 초기값
+	PlayerRole = EPlayerRole::Unassigned ;// 초기값
+	CurrentArrests = 0;
+	MaxArrests = 0;
 }
-
 void AMainPlayerState::SetPlayerRole(EPlayerRole NewRole)
+{
+	if (HasAuthority())
+	{
+		PlayerRole = NewRole;
+	}
+	else
+	{
+		ServerSetPlayerRole(NewRole);
+	}
+}
+void AMainPlayerState::ServerSetPlayerRole_Implementation(EPlayerRole NewRole)
 {
 	PlayerRole = NewRole;
 }
 
+bool AMainPlayerState::ServerSetPlayerRole_Validate(EPlayerRole NewRole)
+{
+	return true;
+}
 void AMainPlayerState::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
 {
-	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
-	DOREPLIFETIME(AMainPlayerState, PlayerRole);
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps); DOREPLIFETIME(AMainPlayerState, PlayerRole);
 }
