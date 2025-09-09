6bb413dce2f096c834d751171622d05df53a443c
LeeSh-213 <hasanghyun66@naver.com>
2025-09-09 19:52:53 +0900

    충돌 해결한 작업본 커밋

--- Source/CH4Project/CH4Project.Build.cs
+++ Source/CH4Project/CH4Project.Build.cs
@@ -8,7 +8,7 @@ public class CH4Project : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "NavigationSystem" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
