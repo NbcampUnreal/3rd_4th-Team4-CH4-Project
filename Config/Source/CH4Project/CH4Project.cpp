6a3c0cc8e1e8f9b1cc187476f6453adbae2ff20a
Hyunsu Jung <freedomquest88@naver.com>
2025-09-18 18:33:55 +0900

    인 게임 맵 수정 : 계단 주변부에 펜스까지 설치하고, 샹들리에 포함 조명 에셋들 추가하고, 1층 로비부터 내부 인테리어 작업 시작
    Config 폴더에 있는 잘못 들어간 파일들 정리

--- Config/Source/CH4Project/CH4Project.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "CH4Project.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CH4Project, "CH4Project" );
