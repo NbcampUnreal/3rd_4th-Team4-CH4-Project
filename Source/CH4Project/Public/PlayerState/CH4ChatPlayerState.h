623c32b4107276fa48c9a5676cc1011ceb084c0c
BaitOtter <hsw9097@gmail.com>
2025-09-26 19:28:38 +0900

    로비 플레이어리스트 제작 중

--- Source/CH4Project/Public/PlayerState/CH4ChatPlayerState.h
+++ Source/CH4Project/Public/PlayerState/CH4ChatPlayerState.h
@@ -4,6 +4,7 @@
 #include "GameFramework/PlayerState.h"
 #include "CH4ChatPlayerState.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnReadyStateChanged, bool, bIsReady);
 
 UCLASS()
 class CH4PROJECT_API ACH4ChatPlayerState : public APlayerState
@@ -19,12 +20,16 @@ public:
     UFUNCTION(Server, Reliable, WithValidation)
     void ServerSetReady(bool bNewReady);
 
+    UPROPERTY(BlueprintAssignable, Category = "Lobby")
+    FOnReadyStateChanged OnReadyStateChanged;
+
 protected:
     UPROPERTY(ReplicatedUsing = OnRep_IsReady, BlueprintReadOnly, Category = "Lobby")
     bool bIsReady = false;
 
     UFUNCTION()
     void OnRep_IsReady();
+
 
     virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
 };
